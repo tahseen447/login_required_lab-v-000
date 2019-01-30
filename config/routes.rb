5cd6f8539b4477c07254342e6adb729a5fc7dde9
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-01-30 23:24:18 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,9 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  root 'application#hello'
+get '/login' => 'sessions#new'
+post '/login' => 'sessions#create'
+post '/logout' => 'sessions#destroy'
 end
