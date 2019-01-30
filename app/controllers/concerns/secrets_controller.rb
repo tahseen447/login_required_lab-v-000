49b432e2c5ed6185bea96275082e7ed0b6f95c6d
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-01-30 23:22:36 +0000

    Automatically backed up by Learn

--- app/controllers/concerns/secrets_controller.rb
+++ app/controllers/concerns/secrets_controller.rb
@@ -1,6 +1,8 @@
 class SecretsController< ApplicationController
   before_action :require_login
 
+  def show
+  end
 private
 def require_login
   return head(:forbidden) unless session.include? :user_id
